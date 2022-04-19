1be4b95b11989235bde84de4031a3963b84ed1ad
Nnanna Okoli <41479644+Nnanna-okoli@users.noreply.github.com>
2022-04-19 11:51:59 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,54 @@
+var today = new Date()
+var curHr = today.getHours()
+
+//Name
+function myFunction() {
+    let person = prompt("Hey there, whats your name?", );
+    if (person != null) {
+      document.getElementById("name").innerHTML =
+      ` ${person},`;
+    }
+  }
+
+  //Greeting
+  function greeting() {
+    if (curHr < 12) {
+      document.getElementById("greeting").innerHTML = `Good morning`;
+    } else if (curHr < 18) {
+      document.getElementById("greeting").innerHTML = `Good afternoon`;
+    } else {
+      document.getElementById("greeting").innerHTML = `Good evening`;
+    }
+
+  };
+greeting()
+
+//Slider
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
